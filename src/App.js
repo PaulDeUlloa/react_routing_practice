f73fdfec536e0052403de9eb487115b91f1ecb6a
Paul De Ulloa <pauld143@gmail.com>
2023-09-06 00:09:17 -0700

    File names updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Home from "./components/Home";
+import Number from "./components/Number";
+import Word from "./components/Word";
+import Colored from "./components/Colored";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/number" element={<Number />} />
+        <Route path="/word" element={<Word />} />
+        <Route path="/colored" element={<Colored />} />
+      </Routes>
     </div>
   );
 }
